22d17f6d4f7256535a2dadd85afc0771144f0286
Arseny Korolkov <94906176+arsykor@users.noreply.github.com>
2025-11-28 14:03:38 +0700

    Working on server configuration

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,62 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func handlePost(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	log.Println(string(body))
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+
+	// Тестовый ответ
+	w.Write([]byte("http://localhost:8080/EwHXdJfB"))
+}
+
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	if id == "" {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", "https://practicum.yandex.ru/")
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func handlerShortner(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		handlePost(w, r)
+	case http.MethodGet:
+		handleGet(w, r)
+	default:
+		http.Error(w, "Bad request", http.StatusBadRequest)
+	}
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", handlerShortner)
+
+	log.Printf("Server starting on http://localhost:8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
+	}
+}

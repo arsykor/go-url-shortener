ee427af7363537e8aa44daaa6f09e7b0a7ec4dab
Arseny Korolkov <94906176+arsykor@users.noreply.github.com>
2025-12-12 13:17:56 +0700

    Add env params

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,19 +2,27 @@ package config
 
 import (
 	"flag"
+	"github.com/caarlos0/env/v6"
 )
 
 // Config holds application configuration
 type Config struct {
-	ServerAddress string
-	BaseURL       string
+	ServerAddress string `env:"SERVER_ADDRESS"`
+	BaseURL       string `env:"BASE_URL"`
 }
 
 func Load() *Config {
 	cfg := &Config{}
 
-	flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "HTTP server address")
-	flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for shortened URLs")
+	_ = env.Parse(cfg)
+
+	if cfg.ServerAddress == "" {
+		flag.StringVar(&cfg.ServerAddress, "a", "localhost:8080", "HTTP server address")
+	}
+
+	if cfg.BaseURL == "" {
+		flag.StringVar(&cfg.BaseURL, "b", "http://localhost:8080", "Base URL for shortened URLs")
+	}
 
 	flag.Parse()
 

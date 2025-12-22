3465a10fe0aef9546301e43c9f00c565f5e974f9
Arseny Korolkov <94906176+arsykor@users.noreply.github.com>
2025-12-22 13:12:38 +0700

    Add middleware:
    * logging HTTP requests
    * and responses

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/arsykor/go-url-shortener/internal/config"
 	"github.com/arsykor/go-url-shortener/internal/handler"
 	"github.com/arsykor/go-url-shortener/internal/repository"
 	"github.com/arsykor/go-url-shortener/internal/service"
+	"go.uber.org/zap"
 )
 
 func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer logger.Sync()
+
+	sugar := logger.Sugar()
 	cfg := config.Load()
 
 	urlRepo := repository.NewInMemoryURLRepository()
 	shortenerService := service.NewShortenerService(urlRepo, cfg.BaseURL)
-	shortenerHandler := handler.NewShortener(shortenerService)
+	shortenerHandler := handler.NewShortener(shortenerService, sugar)
 
 	r := shortenerHandler.Router()
 
-	log.Printf("Server starting on %s", cfg.ServerAddress)
+	sugar.Infow(
+		"Starting server",
+		"addr", cfg.ServerAddress,
+	)
 	if err := http.ListenAndServe(cfg.ServerAddress, r); err != nil {
-		log.Fatal(err)
+		sugar.Fatalw(err.Error(), "event", "start server")
 	}
 }

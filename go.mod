621fa510064fa55179126d899e747e5acbe90321
Arseny Korolkov <94906176+arsykor@users.noreply.github.com>
2026-01-18 18:18:49 +0700

    Add auto migrations up

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.0
 require (
 	github.com/caarlos0/env/v6 v6.10.1
 	github.com/go-chi/chi/v5 v5.2.3
+	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/google/uuid v1.6.0
 	github.com/lib/pq v1.10.9
 	github.com/stretchr/testify v1.11.1
--- internal/database/db.go
+++ internal/database/db.go
@@ -3,7 +3,11 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"path/filepath"
 
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	_ "github.com/lib/pq"
 )
 
@@ -27,5 +31,36 @@ func NewDB(dsn string) (*DB, error) {
 		return nil, fmt.Errorf("failed to ping database: %w", err)
 	}
 
+	if err := runMigrations(db, "migrations"); err != nil {
+		db.Close()
+		return nil, fmt.Errorf("failed to run migrations: %w", err)
+	}
+
 	return &DB{DB: db}, nil
+}
+
+func runMigrations(db *sql.DB, migrationsPath string) error {
+	path := filepath.ToSlash(migrationsPath)
+
+	migrationURL := "file://" + path
+
+	driver, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		return fmt.Errorf("failed to create postgres driver: %w", err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		migrationURL,
+		"postgres",
+		driver,
+	)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate instance: %w", err)
+	}
+
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		return fmt.Errorf("failed to run migrations: %w", err)
+	}
+
+	return nil
 }

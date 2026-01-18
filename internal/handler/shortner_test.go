ab92ee3caffc56dd3987036b6f6ba53e54853cbf
Arseny Korolkov <94906176+arsykor@users.noreply.github.com>
2026-01-18 14:16:04 +0700

    Minor fix

--- internal/handler/shortner_test.go
+++ internal/handler/shortner_test.go
@@ -186,7 +186,7 @@ func TestHandlerShortener_UnsupportedMethod(t *testing.T) {
 	repo := repository.NewInMemoryURLRepository()
 	svc := service.NewShortenerService(repo, "http://localhost:8080")
 	logger := zap.NewNop().Sugar()
-	handler := NewShortener(svc)
+	handler := NewShortener(svc, nil)
 	r := handler.Router(logger)
 
 	req := httptest.NewRequest(http.MethodPut, "/", nil)
